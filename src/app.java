214705cdf7c1a46f42975104bb969eef5e4cc1fe
Nandini Singh <147488335+nandeanie@users.noreply.github.com>
2026-05-19 16:08:45 +0530

    Update app.java

--- src/app.java
+++ src/app.java
@@ -3,6 +3,7 @@ package quizapplication.src;
 import javax.swing.SwingUtilities;
 import quizapplication.src.database.category;
 
+
 public class app {
     public static void main(String[] args) {
         // ensure Swing GUI tasks are executed on the event dispatch thread
